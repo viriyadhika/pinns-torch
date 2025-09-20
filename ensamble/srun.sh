8a831d90f44760ae34a1c401a8953103d2d7ef46
viriyadhika <viriya.dhika0@gmail.com>
2025-09-19 23:12:52 -0400

    fix run

--- ensamble/srun.sh
+++ ensamble/srun.sh
@@ -1,1 +1,1 @@
-sbatch --partition=gpunodes --nodelist=gpunode34 -c 1 --mem=4G --gres=gpu:1 -t 120 run.sh
+sbatch --partition=gpunodes --nodelist=gpunode34 -c 1 --mem=4G --gres=gpu:1 -t 120 ensamble/run.sh
